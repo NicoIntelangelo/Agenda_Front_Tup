f5071e37f0fe7f1ba8a4edc2901abd736390319a
batufantin02 <112518084+batufantin02@users.noreply.github.com>
2022-09-20 18:30:54 -0300

    New contact-card

--- src/app/public/pages/contacts/contacts.module.ts
+++ src/app/public/pages/contacts/contacts.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContactsComponent } from './contacts.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContactsComponent
+  ],
   imports: [
     CommonModule
   ]
