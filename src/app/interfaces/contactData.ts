fabe76ad897fd791187fec13965e140d2054b495
Nico Intelangelo <nicointelangelo1@gmail.com>
2022-09-27 16:09:18 -0300

    creacion contact card/ mockdata/ interface contact

--- src/app/interfaces/contactData.ts
+++ src/app/interfaces/contactData.ts
@@ -1,8 +1,21 @@
+// export interface Contact {
+//     id: number;
+//     first_name: string;
+//     last_name: string;
+//     email: string;
+//     gender: string;
+//     ip_address: string;
+// }
+
+// CONTACTOS DE PRUEBA INSTANCIADOS EN ASSETS MOCK DATA
 export interface Contact {
-    id: number;
-    first_name: string;
-    last_name: string;
-    email: string;
-    gender: string;
-    ip_address: string;
+  firstName: string,
+  lastName: string,
+  phoneNumber: string,
+}
+
+export const defaultContact:Contact = {
+  firstName: "First name",
+  lastName: "Last Name",
+  phoneNumber: "+5412345678"
 }
