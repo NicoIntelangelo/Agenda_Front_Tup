7d0bda7f485cfa877d5ea40cb6ea874cd1071202
Nico Intelangelo <nicointelangelo1@gmail.com>
2022-12-05 16:00:23 -0300

    correccion reload

--- src/app/public/components/contact-card/contact-card.component.ts
+++ src/app/public/components/contact-card/contact-card.component.ts
@@ -30,12 +30,10 @@ export class ContactCardComponent implements OnInit {
   }
 
 
-  deleteContacto(id:number){  //metodo llamado desde el boton borrar del html(recibe el id del contacto de esa agenda)
+  async deleteContacto(id:number){  //metodo llamado desde el boton borrar del html(recibe el id del contacto de esa agenda)
     console.log("contacto id: ",id," eliminado")
-    this.cc.deleteContacto(id) //ejecuta el metodo deleteContacto() del contact component
-    setTimeout(()=>{
-      this.cc.reload();
-    },100);//recarga la ultima agenda, con un retraso de 50mls para asegurarnos q los metodos anteriores hayan terminado
+    await this.cc.deleteContacto(id) //ejecuta el metodo deleteContacto() del contact component
+    this.cc.reload();//recarga la ultima agenda cuando los metodos anteriores hayan terminado
   }
 
   editContacto(id:number){  //metodo llamado desde el boton edit del html(recibe el id del contacto de esa agenda)
